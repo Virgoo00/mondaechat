ccc3c5db595d9912726cca3336b582a4ed6c790e
Elia Roysandi Manurun <118820711+Elroy71@users.noreply.github.com>
2024-06-02 20:46:30 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mondaechat/screens/onboarding/onboarding_screen.dart';
+import 'package:mondaechat/screens/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -39,3 +39,47 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+// KODE LAMA
+// import 'package:flutter/material.dart';
+// import 'package:mondaechat/screens/onboarding/onboarding_screen.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'MONDAEAI',
+//       theme: ThemeData(
+//         fontFamily: 'Poppins',
+//         textTheme: const TextTheme(
+//           bodyLarge: TextStyle(
+//               fontSize: 16.0,
+//               fontFamily: 'Poppins',
+//               fontWeight: FontWeight.normal),
+//           bodyMedium: TextStyle(
+//               fontSize: 14.0,
+//               fontFamily: 'Poppins',
+//               fontWeight: FontWeight.normal),
+//           displayLarge: TextStyle(
+//               fontSize: 24.0,
+//               fontFamily: 'Poppins',
+//               fontWeight: FontWeight.bold),
+//           displayMedium: TextStyle(
+//               fontSize: 22.0,
+//               fontFamily: 'Poppins',
+//               fontWeight: FontWeight.bold),
+//           // Tambahkan gaya teks lainnya sesuai kebutuhan
+//         ),
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const OnboardingScreen(),
+//     );
+//   }
+// }
